940e154196cea826073086cfae38a3d0b7a044eb
ozzy-cydeo <ozzy@cybertekschool.com>
2022-07-19 21:21:35 -0400

    Decomposed

--- project-service/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ project-service/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -109,6 +109,7 @@ public class ProjectServiceImpl implements ProjectService {
     public List<ProjectDTO> listAllProjectDetails(String userName) throws ProjectServiceException {
 
         UserResponseDTO userResponseDto = userClientService.getUserDTOByUserName(userName);
+
         UserDTO user = userResponseDto.getData();
 
         if(user != null){

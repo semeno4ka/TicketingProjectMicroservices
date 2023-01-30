856e4e4a51f17017e71adee9bbe6890835eb7d01
Ekaterina <ekaterina.sementeva@gmail.com>
2023-01-30 14:41:35 -0500

    FeignClient consuming API update

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+@FeignClient(value = "user-service")
+//,url = "http://localhost:9090" specifying the url of user-service is no longer needed you just go to user-service registered with eureka
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
